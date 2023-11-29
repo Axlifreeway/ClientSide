a72537e4c04d5107866e48b71b10c8b77aa25550
OlegsanderF <144331027+OlegsanderF@users.noreply.github.com>
2023-11-29 22:30:16 +0500

    Добавьте файлы проекта.

---
+++ ClietnSide/ClientFacade.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClietnSide
+{
+    public class ClientFacade
+    {
+        public void AddContract(string contractModel)
+        {
+
+        }
+
+        public void DeleteContract(string contractDeleteModel)
+        {
+
+        }
+
+        public void EditContract(string contractModel)
+        {
+
+        }
+
+        public List<string> GetContracts(string[] filter)
+        {
+            return null;
+        }
+
+        public void AddOrganisation(string organisationModel)
+        {
+
+        }
+
+        public void DeleteOrganisation(string organisationDeleteModel)
+        {
+
+        }
+
+        public void EditOrganisation(string organisationModel)
+        {
+
+        }
+
+        public List<string> GetOrganisation(string[] filter)
+        {
+            return null;
+        }
+
+        public void AddVaccination(string vaccinationModel)
+        {
+
+        }
+
+        public void DeleteVaccination(string vaccinationDeleteModel)
+        {
+
+        }
+
+        public void EditVaccination(string vaccinationModel)
+        {
+
+        }
+
+        public List<string> GetVaccination(string[] filter)
+        {
+            return null;
+        }
+
+        public void AddInspection(string inspectionModel)
+        {
+
+        }
+
+        public void DeleteInspection(string inspectionDeleteModel)
+        {
+
+        }
+
+        public void EditInspection(string inspectionModel)
+        {
+
+        }
+
+        public List<string> GetInspection(string[] filter)
+        {
+            return null;
+        }
+    }
+}
---
+++ ClietnSide/MainForm.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClietnSide
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void ClickButtonAddOrganisation()
+        {
+
+        }
+
+        public void ClickButtonRemoveOrganisation()
+        {
+
+        }
+
+        public void ClickButtonEditOrganisation()
+        {
+
+        }
+
+        private bool IsCorrectOrganisation(string organisation)
+        {
+            return true;
+        }
+
+        public void ClickButtonGetOrganisations(DateTime start, DateTime end)
+        {
+
+        }
+
+        public void ClickButtonAddContract()
+        {
+
+        }
+
+        public void ClickButtonRemoveContract()
+        {
+
+        }
+
+        public void ClickButtonEditContract()
+        {
+
+        }
+
+        public bool IsCorrectContract(string contract)
+        {
+            return true;
+        }
+
+        public void ClickButtonGetContracts(DateTime start, DateTime end)
+        {
+
+        }
+
+
+        public void ClickButtonAddVaccination()
+        {
+
+        }
+
+        public void ClickButtonRemoveVaccination()
+        {
+
+        }
+
+        public void ClickButtonEditVaccination()
+        {
+
+        }
+
+        public void isCorrectVaccination()
+        {
+
+        }
+
+        public void ClickButtonGetVacciantions()
+        {
+
+        }
+
+        public void ClickButtonShowOrganisation()
+        {
+
+        }
+
+        public void ClickButtonShowContract()
+        {
+
+        }
+
+        public void ClickButtonShowVaccination()
+        {
+
+        }
+    }
+}
